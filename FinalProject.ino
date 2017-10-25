656c83cf9699a8296dfbc97626af652386d10b0d
JonathanCuevas <33070751+JonathanCuevas@users.noreply.github.com>
2017-10-25 14:12:28 -0400

    Initial commit

---
+++ FinalProject.ino
@@ -0,0 +1,12 @@
+int ledPin = 20;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
